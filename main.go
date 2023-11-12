884caa879bfb9f4ab1db733531e448028705d5a1
Matchima Klomkasem <88373747+Mindo1@users.noreply.github.com>
2023-11-12 10:10:36 +0000

    connect postgres db

--- main.go
+++ main.go
@@ -1,27 +1,79 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
 )
 
-func HandleIndex(c echo.Context) error {
-	name := c.QueryParam("name")
-	return c.String(http.StatusOK, "Hello World, "+name)
+// Connect DB
+
+const (
+	DB_NAME     = "greekybase"
+	DB_USER     = "postgres"
+	DB_PASSWORD = "mypassword"
+	DB_HOST     = "host.docker.internal"
+)
+
+type Member struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
 }
 
+var db *sql.DB
+
+func init() {
+
+	var err error
+
+	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Println("Error opening database connection:", err)
+		return
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Error pinging database:", err)
+		return
+	}
+
+	fmt.Println("Connected to the database")
+}
+
+// Handler functions
+func GetMembers(c echo.Context) error {
+	rows, err := db.Query("SELECT id, username, email FROM members")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to fetch members"})
+	}
+	defer rows.Close()
+
+	var members []Member
+	for rows.Next() {
+		var member Member
+		err := rows.Scan(&member.ID, &member.Username, &member.Email)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to scan members"})
+		}
+		members = append(members, member)
+	}
+
+	return c.JSON(http.StatusOK, members)
+}
+
+// Start the Echo server
 func main() {
 	e := echo.New()
 
-	// ===== before refactor
-	// e.GET("/", func(c echo.Context) error {
-	//     return c.String(http.StatusOK, "Hello, World!")
-	// })
+	// Routes
+	e.GET("/", GetMembers)
 
-	// ===== after refactor
-	e.GET("/", HandleIndex)
-	e.POST("/", HandleIndex)
-
-	e.Logger.Fatal(e.Start(":1323"))
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
 }

a277a8e696e64e187e2ebeaab2af9bdc1baca00a
Matchima Klomkasem <88373747+Mindo1@users.noreply.github.com>
2023-11-12 07:46:04 +0000

    GO Routine & Channel

--- main.go
+++ main.go
@@ -1,38 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+// GO Routine & Channel
 
-// Interface
-
-type SaveLogInterface interface {
-	Savelog()
+func Ping1S(c chan int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println("Ping: ", i)
+		time.Sleep(1 * time.Second)
+	}
+	c <- 20 //channel ตัวแปรที่รับเข้ามาใน func
 }
 
-func Savelog(st SaveLogInterface) {
-	st.Savelog()
-}
-
-type TransferBBL struct {
-	Name string
-}
-
-func (tBBL *TransferBBL) Savelog() {
-	tBBL.Name = "test"
-	fmt.Println("Save to database ", tBBL.Name)
-}
-
-type TransferKTB struct {
-	Name string
-}
-
-func (tKTB TransferKTB) Savelog() {
-	fmt.Println("Save to database ", tKTB.Name)
+func SendNoti5S(ch chan string) {
+	fmt.Println("send noti")
+	time.Sleep(5 * time.Second)
+	fmt.Println("send noti")
+	ch <- "suces"
 }
 
 func main() {
-	transferA := TransferBBL{Name: "BBL"}
-	transferB := TransferKTB{Name: "KTB"}
-	Savelog(&transferA)
-	Savelog(transferB)
+	c := make(chan int)
+	ch := make(chan string)
+	go Ping1S(c) //run function con-current ทำงานไปพร้อมกัน โดยไม่ต้องรอให้บรรทัดไหนเสร็จก่อน
+	go SendNoti5S(ch)
+	time.Sleep(10 * time.Second)
+
+	pingValue, SendNotiValue := <-c, <-ch
+	fmt.Println(pingValue, SendNotiValue) //print ค่า ch ที่บอกว่า function ทำงานเสร็จเมื่อไหร่
+	fmt.Println("completed!")
 }

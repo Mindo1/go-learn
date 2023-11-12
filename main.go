a633fc7fbd0055e4c9c0b85a2266a31aad3f8ed6
Matchima Klomkasem <88373747+Mindo1@users.noreply.github.com>
2023-11-12 04:23:49 +0000

    fucntion

--- main.go
+++ main.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("test")
+func GetMember() {
+	fmt.Println("please wait...")
+}
+
+func IsInSyetem(username string) bool {
+	return true
+}
+
+func GetUserDetail(username string) (int, string) {
+	
+	return 201, "manager"
+}
+
+func GetDeparture(username string, departure *string) {
+	if username != ""{
+		*departure = "home"
+	}
+}
+
+func CheckLogin (username string, password string) {
+	if IsInSyetem(username) {
+		fmt.Println("found user in system")
+		GetUserDetail(username)
+		departure := ""
+		GetDeparture(username, &departure)
+		fmt.Println(departure)
+	}
+}
+
+func main(){
+	GetMember()
 }

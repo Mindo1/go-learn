dd89275a49af50ed82be8ff4a1b3278c202545e1
Matchima Klomkasem <88373747+Mindo1@users.noreply.github.com>
2023-11-12 08:50:24 +0000

    go web service

--- main.go
+++ main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
-	"myapp/libs"
+	"io"
+	"log"
+	"net/http"
 )
 
+func FuncIndex(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello World, ")
+	fmt.Println(r.Method)
+	// การเขียน query param http://localhost:8000/?name=mind
+	fmt.Println(r.URL.Query().Get("name"))
+	if r.Method == "GET" {
+		io.WriteString(w, "hello world this is get method")
+	} else {
+		io.WriteString(w, "hello world this is other method")
+	}
+
+}
+
 func main() {
-	fmt.Println("test on main call greeting function")
-	libs.Greeting()
+	http.HandleFunc("/", FuncIndex)
+	fmt.Println("start server completed")
+	error := http.ListenAndServe(":8000", nil)
+	if error != nil {
+		log.Fatal(error)
+	}
 }
